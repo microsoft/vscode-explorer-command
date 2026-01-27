3118b130f7ea036a6b98e5cdf943c306b88922fe
deepak1556 <hop2deep@gmail.com>
2026-01-27 20:43:35 +0900

    fix: icon and exe path for stable with versioned layout

--- src/explorer_command.cc
+++ src/explorer_command.cc
@@ -166,11 +166,7 @@ class __declspec(uuid(DLL_UUID)) ExplorerCommandHandler final : public RuntimeCl
 
   IFACEMETHODIMP GetIcon(IShellItemArray* items, PWSTR* icon) {
     std::filesystem::path module_path{ wil::GetModuleFileNameW<std::wstring>(wil::GetModuleInstanceHandle()) };
-#if defined(INSIDER)
     module_path = module_path.remove_filename().parent_path().parent_path().parent_path();
-#else
-    module_path = module_path.remove_filename().parent_path().parent_path();
-#endif
     module_path /= EXE_NAME;
     return SHStrDupW(module_path.c_str(), icon);
   }
@@ -203,11 +199,7 @@ class __declspec(uuid(DLL_UUID)) ExplorerCommandHandler final : public RuntimeCl
   IFACEMETHODIMP Invoke(IShellItemArray* items, IBindCtx* bindCtx) {
     if (items) {
       std::filesystem::path module_path{ wil::GetModuleFileNameW<std::wstring>(wil::GetModuleInstanceHandle()) };
-#if defined(INSIDER)
       module_path = module_path.remove_filename().parent_path().parent_path().parent_path();
-#else
-      module_path = module_path.remove_filename().parent_path().parent_path();
-#endif
       module_path /= EXE_NAME;
       DWORD count;
       RETURN_IF_FAILED(items->GetCount(&count));
